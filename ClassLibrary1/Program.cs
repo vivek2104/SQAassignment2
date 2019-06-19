321d5d5aa966553a6711d7fe2c0bb4dbfc5f6631
Vivek Katroju <Vkatroju5947@conestogac.on.ca>
2019-06-19 16:04:30 -0400

    updates

--- ClassLibrary1/Program.cs
+++ ClassLibrary1/Program.cs
@@ -27,32 +27,36 @@ namespace ClassLibrary1
                 Console.Write("-------------------------\n");
                 Console.WriteLine("\n1.Enter the Triangle dimensions\n");
                 Console.WriteLine("\n2.Exit \n");
-                Console.Write("------------------------\n");
+              
                 if ((!int.TryParse(Console.ReadLine(), out input)) || input <= 0 || input > 2)
                 {
                     Console.WriteLine("Please Enter the Correct option: ");
                     continue;
                 }
-
+                    Console.Write("------------------------\n");
+                //gets the input of side 1 of a triangle
+                do
+                {
                     Console.Write("Enter Side 1 of a triangle\n");
                     side1 = Convert.ToInt32(Console.ReadLine());
+                } while (side1 <= 0);
 
-                    //gets the input of side 2 of a triangle
 
+                //gets the input of side 2 of a triangle
+                do
+                {
                     Console.Write("Enter Side 2 of a triangle\n");
                     side2 = Convert.ToInt32(Console.ReadLine());
+                } while (side2 <= 0);
 
-                    //gets the input of side 2 of a triangle
-
+                //gets the input of side 3 of a triangle
+                do
+                {
                     Console.Write("Enter Side 3 of a triangle\n");
                     side3 = Convert.ToInt32(Console.ReadLine());
+                } while (side3 <= 0);
 
-                    
-
-
-                
-
-
+                Console.WriteLine(triangleSolver.Analyze(side1, side2, side3));
 
             } while ( input!= 2 );
         }
--- ClassLibrary1/triangleSolver.cs
+++ ClassLibrary1/triangleSolver.cs
@@ -6,8 +6,34 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    class triangelSolver
+    public static class triangleSolver
     {
+        public static string Analyze(int side1, int side2, int side3)
+        {
+            int sSide1 = side1 + side2;
+            int sSide2 = side2 + side3;
+            int sSide3 = side3 + side1;
 
+            if (sSide1 > side3 && sSide2 > side1 && sSide3 > side2)
+            {
+                if (side1 == side2 && side2 == side3)
+                {
+                    Console.Write("This is a equilateral triangle!!!");
+                    return " ";
+                }
+                else if (side1 == side2 || side2 == side3 || side3 == side1)
+                {
+                    Console.Write("This is a isosceles triangle");
+                    return " ";
+                }
+                else
+                {
+                    Console.Write("This is a Scalene triangle");
+                    return " ";
+                } 
+            
+            }
+         else return "Triangle cannot be formed";
+        }
     }
 }
